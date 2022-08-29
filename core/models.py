95c898a3373650db5e7b49d1393a6b82be6ced6e
Paulo <paulorolinski.ifc@gmail.com>
2022-08-28 21:33:09 -0300

    Instalação do black

--- core/models.py
+++ core/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
 
     def __str__(self):
         return self.descricao
+
 
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
