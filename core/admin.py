2507b0b0cdcf47b6b8bcfa2cd2470edc261df91c
Paulo <paulorolinski.ifc@gmail.com>
2022-08-28 21:17:40 -0300

    Aula 2

---
+++ core/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from core.models import Categoria
+
+admin.site.register(Categoria)
---
+++ core/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=100)
+    site = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
