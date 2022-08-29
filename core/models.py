d75eb2e479805f7cc3172486ae6605628bbfddcf
Paulo <paulorolinski.ifc@gmail.com>
2022-08-28 21:45:17 -0300

    Aula 5

--- core/models.py
+++ core/models.py
@@ -14,3 +14,29 @@ class Editora(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name_plural = "Autores"
+
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32, null=True, blank=True)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros"
+    )
+    editora = models.ForeignKey(
+        Editora, on_delete=models.PROTECT, related_name="livros"
+    )
+
+    def __str__(self):
+        return f'{self.titulo} ({self.quantidade})'
