a0307fb5456aaa2e12a83374ca7bfd1b5b9dfc9a
Paulo <paulorolinski.ifc@gmail.com>
2022-09-27 15:07:41 -0300

    Aula 16 - Views organizadas em pastas

--- core/views/livro.py
+++ core/views/livro.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Livro
-from core.serializers import LivroSerializer
+from core.serializers import LivroSerializer, LivroDetailSerializer
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
