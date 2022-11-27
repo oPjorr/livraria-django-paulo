681044cb9e1cdaa0a70144e49fe1602ecc38b7d6
Paulo <paulorolinski.ifc@gmail.com>
2022-11-27 19:08:19 -0300

    Aula 21 - Dump e Load de dados

--- core/models/autor.py
+++ core/models/autor.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Autor(models.Model):
     nome = models.CharField(max_length=255)
-    email = models.EmailField()
+    email = models.EmailField(null=True, blank=True)
 
     def __str__(self):
         return self.nome
