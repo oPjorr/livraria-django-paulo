dd4777d7e64ed2a8a46081f9406fcfcdb188bec5
Paulo <paulorolinski.ifc@gmail.com>
2022-11-27 19:37:48 -0300

    Aula 24 - Configurando variáveis de ambiente

--- livraria/settings.py
+++ livraria/settings.py
@@ -1,9 +1,18 @@
+import environ
 import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Carrega as variáveis de ambiente do sistema operacional e as prepara para usá-las
+env = environ.Env()
+environ.Env.read_env((os.path.join(BASE_DIR, '.env')))
+
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
+DATABASES = {'default': env.db()}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
